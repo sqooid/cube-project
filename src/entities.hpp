71728866951d33e9fc5761d7f99ce92c02ceae5c
sqooid <lucasliu7@hotmail.com>
2021-07-10 23:17:25 +1000

    Improved cube object structure

--- src/entities.hpp
+++ src/entities.hpp
@@ -8,15 +8,59 @@
 #include <glm/gtx/quaternion.hpp>
 #include <glm/mat4x4.hpp>
 
-class Cube
+namespace Cube
 {
-	public:
-        Cube(float radius);
+    static std::vector<GLfloat> vertices{
+        -1.0, -1.0, 1.0,  //0
+        1.0, -1.0, 1.0,   //1
+        -1.0, 1.0, 1.0,   //2
+        1.0, 1.0, 1.0,    //3
+        -1.0, -1.0, -1.0, //4
+        1.0, -1.0, -1.0,  //3
+        -1.0, 1.0, -1.0,  //6
+        1.0, 1.0, -1.0,   //7
+    };
 
-        std::vector<GLfloat> vertices;
-        std::vector<GLuint> faceIndices;
-        std::vector<GLuint> edgeIndices;
-        std::vector<GLfloat> colors;
+    static std::vector<GLuint> faceIndices{
+        //Top
+        2, 7, 6,
+        2, 3, 7,
+        //Bottom
+        5, 0, 4,
+        5, 1, 0,
+        //Left
+        0, 2, 6,
+        0, 6, 4,
+        //Right
+        1, 7, 3,
+        1, 5, 7,
+        //Front
+        3, 2, 0,
+        3, 0, 1,
+        //Back
+        4, 6, 7,
+        4, 7, 5};
+
+    static std::vector<GLuint> edgeIndices{
+        0, 1, 1, 3, 3, 2, 2, 0,
+        1, 5, 3, 7, 2, 6, 0, 4,
+        4, 5, 5, 7, 7, 6, 6, 4};
+
+    static std::vector<GLfloat> colors{
+        1.0, 1.0, 1.0,
+        1.0, 0.0, 0.0,
+        0.0, 1.0, 0.0,
+        0.0, 0.0, 1.0,
+        1.0, 1.0, 0.0,
+        1.0, 0.0, 1.0,
+        0.0, 1.0, 1.0,
+        0.1, 0.1, 0.1};
+
+    class Cube
+    {
+    public:
+        Cube(float radius, glm::vec3 position, glm::vec3 orientation);
+
         glm::vec3 position;
         glm::quat rotation;
         float size;
@@ -26,6 +70,7 @@ class Cube
         void scale(float diff);
 
         glm::mat4 genModelMatrix();
-};
+    };
+}
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -12,25 +12,39 @@
 
 GLenum glCheckError_(const char *file, int line)
 {
-    GLenum errorCode;
-    while ((errorCode = glGetError()) != GL_NO_ERROR)
-    {
-        std::string error;
-        switch (errorCode)
-        {
-            case GL_INVALID_ENUM:                  error = "INVALID_ENUM"; break;
-            case GL_INVALID_VALUE:                 error = "INVALID_VALUE"; break;
-            case GL_INVALID_OPERATION:             error = "INVALID_OPERATION"; break;
-            case GL_STACK_OVERFLOW:                error = "STACK_OVERFLOW"; break;
-            case GL_STACK_UNDERFLOW:               error = "STACK_UNDERFLOW"; break;
-            case GL_OUT_OF_MEMORY:                 error = "OUT_OF_MEMORY"; break;
-            case GL_INVALID_FRAMEBUFFER_OPERATION: error = "INVALID_FRAMEBUFFER_OPERATION"; break;
-        }
-        std::cout << error << " | " << file << " (" << line << ")" << std::endl;
-    }
-    return errorCode;
+	GLenum errorCode;
+	while ((errorCode = glGetError()) != GL_NO_ERROR)
+	{
+		std::string error;
+		switch (errorCode)
+		{
+		case GL_INVALID_ENUM:
+			error = "INVALID_ENUM";
+			break;
+		case GL_INVALID_VALUE:
+			error = "INVALID_VALUE";
+			break;
+		case GL_INVALID_OPERATION:
+			error = "INVALID_OPERATION";
+			break;
+		case GL_STACK_OVERFLOW:
+			error = "STACK_OVERFLOW";
+			break;
+		case GL_STACK_UNDERFLOW:
+			error = "STACK_UNDERFLOW";
+			break;
+		case GL_OUT_OF_MEMORY:
+			error = "OUT_OF_MEMORY";
+			break;
+		case GL_INVALID_FRAMEBUFFER_OPERATION:
+			error = "INVALID_FRAMEBUFFER_OPERATION";
+			break;
+		}
+		std::cout << error << " | " << file << " (" << line << ")" << std::endl;
+	}
+	return errorCode;
 }
-#define glCheckError() glCheckError_(__FILE__, __LINE__) 
+#define glCheckError() glCheckError_(__FILE__, __LINE__)
 
 void initialize()
 {
@@ -40,24 +54,24 @@ void initialize()
 	// glEnable(GL_BLEND);
 
 	// OpenGL version printing
-	const GLubyte *renderer = glGetString( GL_RENDERER ); 
-	const GLubyte *vendor = glGetString( GL_VENDOR ); 
-	const GLubyte *version = glGetString( GL_VERSION ); 
-	const GLubyte *glslVersion = glGetString( GL_SHADING_LANGUAGE_VERSION ); 
-	GLint major, minor; 
-	glGetIntegerv(GL_MAJOR_VERSION, &major); 
-	glGetIntegerv(GL_MINOR_VERSION, &minor); 
-	
-	printf("GL Vendor            : %s\n", vendor); 
-	printf("GL Renderer          : %s\n", renderer); 
-	printf("GL Version (string)  : %s\n", version); 
-	printf("GL Version (integer) : %d.%d\n", major, minor); 
+	const GLubyte *renderer = glGetString(GL_RENDERER);
+	const GLubyte *vendor = glGetString(GL_VENDOR);
+	const GLubyte *version = glGetString(GL_VERSION);
+	const GLubyte *glslVersion = glGetString(GL_SHADING_LANGUAGE_VERSION);
+	GLint major, minor;
+	glGetIntegerv(GL_MAJOR_VERSION, &major);
+	glGetIntegerv(GL_MINOR_VERSION, &minor);
+
+	printf("GL Vendor            : %s\n", vendor);
+	printf("GL Renderer          : %s\n", renderer);
+	printf("GL Version (string)  : %s\n", version);
+	printf("GL Version (integer) : %d.%d\n", major, minor);
 	printf("GLSL Version         : %s\n", glslVersion);
 
 	glewInit();
 }
 
-void loadData(Cube &cube, std::vector<GLuint> &vao)
+void loadData(Cube::Cube &cube, std::vector<GLuint> &vao)
 {
 	// Loading cube data
 	glBindVertexArray(vao[0]);
@@ -65,7 +79,7 @@ void loadData(Cube &cube, std::vector<GLuint> &vao)
 	GLuint vertexBuffer;
 	glGenBuffers(1, &vertexBuffer);
 	glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * cube.vertices.size(), &(cube.vertices[0]), GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * Cube::vertices.size(), &(Cube::vertices[0]), GL_STATIC_DRAW);
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 	glCheckError();
@@ -73,7 +87,7 @@ void loadData(Cube &cube, std::vector<GLuint> &vao)
 	GLuint colorBuffer;
 	glGenBuffers(1, &colorBuffer);
 	glBindBuffer(GL_ARRAY_BUFFER, colorBuffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * cube.colors.size(), &(cube.colors[0]), GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * Cube::colors.size(), &(Cube::colors[0]), GL_STATIC_DRAW);
 	glEnableVertexAttribArray(1);
 	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);
 	glCheckError();
@@ -81,7 +95,7 @@ void loadData(Cube &cube, std::vector<GLuint> &vao)
 	GLuint indexBuffer;
 	glGenBuffers(1, &indexBuffer);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint) * cube.faceIndices.size(), &(cube.faceIndices[0]), GL_STATIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint) * Cube::faceIndices.size(), &(Cube::faceIndices[0]), GL_STATIC_DRAW);
 	glCheckError();
 
 	// Edges
@@ -95,7 +109,7 @@ void loadData(Cube &cube, std::vector<GLuint> &vao)
 	GLuint edgeIndexBuffer;
 	glGenBuffers(1, &edgeIndexBuffer);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, edgeIndexBuffer);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint) * cube.edgeIndices.size(), &(cube.edgeIndices[0]), GL_STATIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint) * Cube::edgeIndices.size(), &(Cube::edgeIndices[0]), GL_STATIC_DRAW);
 	glCheckError();
 
 	glBindVertexArray(0);
@@ -112,7 +126,7 @@ void compileShaders(std::vector<GLuint> &shaders)
 	glLinkProgram(faceProgram);
 	GLint status;
 	glGetProgramiv(faceProgram, GL_LINK_STATUS, &status);
-	if ( status == GL_FALSE )
+	if (status == GL_FALSE)
 	{
 		std::cout << "Fucking problemo with face program (1)\n";
 	}
@@ -121,15 +135,15 @@ void compileShaders(std::vector<GLuint> &shaders)
 	// Edge shader
 	GLuint edgeProgram = LoadShader("shaders/edge.vert", "shaders/edge.frag");
 	glLinkProgram(edgeProgram);
-		glGetProgramiv(faceProgram, GL_LINK_STATUS, &status);
-	if ( status == GL_FALSE )
+	glGetProgramiv(faceProgram, GL_LINK_STATUS, &status);
+	if (status == GL_FALSE)
 	{
 		std::cout << "Fucking problemo with edge program (1)\n";
 	}
 	shaders[1] = edgeProgram;
 }
 
-void render(std::vector<GLuint> &vao, std::vector<GLuint> &shaders, Cube cube)
+void render(std::vector<GLuint> &vao, std::vector<GLuint> &shaders, Cube::Cube cube)
 {
 	glClearColor(0.1f, 0.1f, 0.1f, 0.0f);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -146,8 +160,6 @@ void render(std::vector<GLuint> &vao, std::vector<GLuint> &shaders, Cube cube)
 
 	glBindVertexArray(0);
 	glUseProgram(0);
-
-	
 }
 
 typedef struct Control
@@ -156,7 +168,7 @@ typedef struct Control
 	sf::Vector2i lastRightClick;
 } Control;
 
-Control tick(sf::Window &window, Cube &cube, Control &controlState)
+Control tick(sf::Window &window, Cube::Cube &cube, Control &controlState)
 {
 	// Control sensitivities
 	float rotationSpeed = 300.0;
@@ -164,9 +176,9 @@ Control tick(sf::Window &window, Cube &cube, Control &controlState)
 
 	sf::Vector2i currPosition = sf::Mouse::getPosition(window);
 	// Left click drag for rotation
-	if(sf::Mouse::isButtonPressed(sf::Mouse::Left))
+	if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
 	{
-		if(controlState.lastLeftClick == sf::Vector2i (-1, -1) || controlState.lastLeftClick == currPosition)
+		if (controlState.lastLeftClick == sf::Vector2i(-1, -1) || controlState.lastLeftClick == currPosition)
 		{
 			controlState.lastLeftClick = currPosition;
 		}
@@ -175,7 +187,7 @@ Control tick(sf::Window &window, Cube &cube, Control &controlState)
 			sf::Vector2i direction = currPosition - controlState.lastLeftClick;
 			glm::vec3 zPos(0.0, 0.0, 1.0);
 			glm::vec3 rotationAxis = glm::cross(zPos, glm::vec3(direction.x, -direction.y, 0.0));
-			float distance = glm::length(rotationAxis) / (float) window.getSize().x * rotationSpeed;
+			float distance = glm::length(rotationAxis) / (float)window.getSize().x * rotationSpeed;
 			std::cout << "Angle: " << distance << "\n";
 
 			rotationAxis = rotationAxis / distance;
@@ -185,63 +197,63 @@ Control tick(sf::Window &window, Cube &cube, Control &controlState)
 	}
 	else
 	{
-		controlState.lastLeftClick = sf::Vector2i (-1, -1);
+		controlState.lastLeftClick = sf::Vector2i(-1, -1);
 	}
 
 	// Right click for movement
-	if(sf::Mouse::isButtonPressed(sf::Mouse::Right))
+	if (sf::Mouse::isButtonPressed(sf::Mouse::Right))
 	{
-		if(controlState.lastRightClick == sf::Vector2i (-1, -1) || controlState.lastRightClick == currPosition)
+		if (controlState.lastRightClick == sf::Vector2i(-1, -1) || controlState.lastRightClick == currPosition)
 		{
 			controlState.lastRightClick = currPosition;
 		}
 		else
 		{
 			sf::Vector2i direction = currPosition - controlState.lastRightClick;
-			glm::vec3 directionVec (direction.x, -direction.y, 0.0);
+			glm::vec3 directionVec(direction.x, -direction.y, 0.0);
 			cube.move(directionVec * movementSpeed);
-			
+
 			controlState.lastRightClick = currPosition;
-		}	
+		}
 	}
 	else
 	{
-		controlState.lastRightClick = sf::Vector2i (-1, -1);
+		controlState.lastRightClick = sf::Vector2i(-1, -1);
 	}
-	
+
 	return controlState;
 }
 
-void handleEvents(sf::Window &window, sf::Event event, Cube &cube)
+void handleEvents(sf::Window &window, sf::Event event, Cube::Cube &cube)
 {
 	// Scale speed
-	float scaleSpeed = 0.05;
+	float zoomSpeed = 0.2;
 
 	// Events
-	switch(event.type)
+	switch (event.type)
 	{
-		case sf::Event::Closed:
-			window.close();
-			break;
+	case sf::Event::Closed:
+		window.close();
+		break;
 
-		case sf::Event::Resized:
-			glViewport(0, 0, (GLsizei) event.size.width, (GLsizei) event.size.height);
-			break;
+	case sf::Event::Resized:
+		glViewport(0, 0, (GLsizei)event.size.width, (GLsizei)event.size.height);
+		break;
 
-		case sf::Event::MouseWheelMoved:
-			float wheelMovement = event.mouseWheel.delta;
-			cube.scale(wheelMovement * scaleSpeed);
-			break;
-	}	
+	case sf::Event::MouseWheelMoved:
+		float wheelMovement = event.mouseWheel.delta;
+		cube.move(wheelMovement * glm::vec3(0.0, 0.0, zoomSpeed));
+		break;
+	}
 }
 
 int main()
 {
 	// Getting Depth Buffer to work
 	sf::ContextSettings Settings;
-	Settings.depthBits         = 24; // Request a 24 bits depth buffer
+	Settings.depthBits = 24; // Request a 24 bits depth buffer
 	// Settings.stencilBits       = 8;  // Request a 8 bits stencil buffer
-	Settings.antialiasingLevel = 2;  // Request 2 levels of antialiasing
+	Settings.antialiasingLevel = 2; // Request 2 levels of antialiasing
 
 	sf::Window window(sf::VideoMode(800, 600), "3D Shenanigans", sf::Style::Default, Settings);
 	window.setActive(true);
@@ -250,11 +262,11 @@ int main()
 	initialize();
 
 	// Loading shaders
-	std::vector<GLuint> shaders (2);
+	std::vector<GLuint> shaders(2);
 	compileShaders(shaders);
 
 	// Loading cube data
-	Cube cube (0.5f);
+	Cube::Cube cube(0.5f, glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 0.0, 1.0));
 	// Creating vao's
 	std::vector<GLuint> vao(2);
 	glGenVertexArrays(2, &vao[0]);
@@ -265,41 +277,35 @@ int main()
 		glm::radians(90.0f),
 		4.0f / 3.0f,
 		0.1f,
-		100.0f
-	);
+		100.0f);
 
-	// Setting view matrix 
+	// Setting view matrix
 	glm::mat4 cameraMatrix = glm::lookAt(
 		glm::vec3(0.0f, 0.0f, 0.0f),
 		glm::vec3(0.0f, 0.0f, -1.0f),
-		glm::vec3(0.0f, 1.0f, 0.0f)
-	);
+		glm::vec3(0.0f, 1.0f, 0.0f));
 
-	std::vector<GLint> MVPLocations (2);
+	std::vector<GLint> MVPLocations(2);
 	MVPLocations[0] = glGetUniformLocation(shaders[0], "MVP");
 	MVPLocations[1] = glGetUniformLocation(shaders[1], "MVP");
-
-	cube.move(glm::vec3 (-1.0, -1.0, -3.0));
 
 	// Clock
 	sf::Clock clock;
 	double t = 0.0;
 	double dt = 1.0 / 144.0;
 	double accumulator = 0.0;
-	Control controlState
-	{
-		sf::Vector2i (-1, -1),
-		sf::Vector2i (-1, -1)
-	};
+	Control controlState{
+		sf::Vector2i(-1, -1),
+		sf::Vector2i(-1, -1)};
 
-	while(window.isOpen())
+	while (window.isOpen())
 	{
 		double frameTime = clock.getElapsedTime().asSeconds();
 		accumulator += frameTime;
 		clock.restart();
-		while(accumulator >= dt)
+		while (accumulator >= dt)
 		{
-			while(window.pollEvent(event))
+			while (window.pollEvent(event))
 			{
 				handleEvents(window, event, cube);
 			}
